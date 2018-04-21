4648964a76180b507143cf2ac6c538428328670d
Danny <dukeydk@gmail.com>
2018-04-20 20:41:29 -0400

    Initial commit

---
+++ execute.py
@@ -0,0 +1,11 @@
+import os
+import IlocSim as sim
+
+def getIloc():
+    with open('iloc.out', 'r') as code:
+        iloc = code.read()
+    return iloc
+
+os.system("make")
+os.system("codegen < testcases/demo8")
+#sim.start(getIloc())
